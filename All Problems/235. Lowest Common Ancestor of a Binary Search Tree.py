bbefad68d7500c6cce3b1526c899b33bd8de99a4
RickLiu <mushiking02134tc@gmail.com>
2022-05-29 09:48:31 +0800

    # Update - Finished no.235 problem

--- All Problems/235. Lowest Common Ancestor of a Binary Search Tree.py
+++ All Problems/235. Lowest Common Ancestor of a Binary Search Tree.py
@@ -3,31 +3,27 @@ from SelfCreateFunction.CreateBinaryTree import CreateBinaryTree
 
 class Solution(object):
     def lowestCommonAncestor(self, root, p, q):
-        if p < root.val and q > root.val:
-            return root.val
+        mininum_val = p.val if p.val < q.val else q.val
+        maxinum_val = p.val if p.val > q.val else q.val
 
-        LCA = root.val
-
-        if root.right and p == root.right.val:
-            LCA = root.val
-
-        if root.left and p == root.left.val:
-            LCA = root.val
-
-        if p < root.val:
-            self.lowestCommonAncestor(root.left if root.left else None, p, q)
-
-        if p > root.val:
-            self.lowestCommonAncestor(root.right if root.right else None, p, q)
-
-        return LCA
+        while True:
+            if mininum_val < root.val < maxinum_val:
+                return root
+            elif p.val == root.val or q.val == root.val:
+                return root
+            elif root.val > maxinum_val:
+                root = root.left
+            else:
+                root = root.right
 
 
 if __name__ == '__main__':
     tree_data = [6, 2, 8, 0, 4, 7, 9, None, None, 3, 5]
+    p_data = [4, 3, 5]
+    q_data = [2, 0, 4]
     i_root = CreateBinaryTree().new_tree(tree_data)
-    i_p = 3
-    i_q = 8
+    i_p = CreateBinaryTree().new_tree(p_data)
+    i_q = CreateBinaryTree().new_tree(q_data)
 
     run = Solution()
-    print(run.lowestCommonAncestor(i_root, i_p, i_q))
+    print(run.lowestCommonAncestor(i_root, i_p, i_q).val)
