5a5c631bad776ea22b99f5aab4573c6d02f42726
Rick Liu <mushiking02134tc@gmail.com>
2022-05-31 08:58:26 +0800

    #Update - Update no.1779 new solution

--- All Problems/1779. Find Nearest Point That Has the Same X or Y Coordinate.py
+++ All Problems/1779. Find Nearest Point That Has the Same X or Y Coordinate.py
@@ -3,22 +3,35 @@ import math
 
 class Solution(object):
     def nearestValidPoint(self, x, y, points):
-        val_index = -1
-        re_val = math.inf
+        vai_points = [point for point in points if point[0] == x or point[1] == y]
+
+        if len(vai_points) == 0:
+            return -1
+        elif len(vai_points) == 1:
+            return points.index(vai_points[0])
+
+        val_list = {points.index(p): abs(p[0] - x) + abs(p[1] - y) for p in vai_points}
+        return [k for k, v in val_list.items() if v == min(val_list.values())][0]
+
+    def little_faster(self, x, y, points):
+        minDist = math.inf
+        ans = -1
         for i in range(len(points)):
             if points[i][0] == x or points[i][1] == y:
-                count_dis = abs(points[i][0] - x) + abs(points[i][1] - y)
-                if count_dis < re_val:
-                    val_index = i
-                    re_val = count_dis
-
-        return val_index
-
+                manDist = abs(points[i][0] - x) + abs(points[i][1] - y)
+                if manDist < minDist:
+                    ans = i
+                    minDist = manDist
+        return ans
 
 if __name__ == '__main__':
-    i_points = [[8205, 8862]]
-    i_x = 58
-    i_y = 7837
+    i_points = [[25, 45], [60, 19], [11, 38], [32, 22], [1, 24], [26, 25], [52, 36], [45, 54], [45, 30], [45, 39],
+                [39, 38],
+                [25, 38], [39, 57], [47, 51], [47, 49], [37, 21], [16, 43], [53, 33], [10, 50], [30, 29], [3, 31],
+                [45, 26],
+                [22, 40], [2, 31], [57, 42], [25, 42], [37, 13], [13, 54], [17, 5], [50, 32]]
+    i_x = 28
+    i_y = 51
 
     run = Solution()
     print(run.nearestValidPoint(i_x, i_y, i_points))
